08680b009b8a16aa32c7449b92f17ec96d1f26a8
peter-gribanov <wcode404@gmail.com>
2013-03-20 23:18:19 +0400

    add autoloader

---
+++ autoload.php
@@ -0,0 +1,21 @@
+<?php
+/**
+ * Microsoft package
+ * 
+ * @package Microsoft
+ * @author  Peter Gribanov <gribanov@professionali.ru>
+ */
+
+
+// автолоадер
+spl_autoload_register(function($name) {
+	$type = strpos($name, 'Interface') === false ? 'class' : 'interface';
+	$is = $type.'_exists';
+	$file = __DIR__.'/'.str_replace(array('_', '\\'), '/', $name).'.php';
+	if (is_readable($file)) {
+		include_once $file;
+		if (!$is($name, false)) {
+			throw new Exception('В файле "'.$file.'" '.($type=='class' ? 'класс' : 'интерфейс').' "'.$name.'" не найден');
+		}
+	}
+});
